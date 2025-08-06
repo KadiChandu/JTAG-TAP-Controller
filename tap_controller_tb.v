3ecdbf3f86dafd1c6163b17f6c3286b24416988b
Kadi Chandu <chandukadi111@gmail.com>
2025-08-06 11:52:45 +0530

    Update tap_controller_tb.v

--- tap_controller_tb.v
+++ tap_controller_tb.v
@@ -1,68 +1,42 @@
+`timescale 1ns / 1ps
 module tap_controller_tb;
-	reg tck;
-	reg tms;
-	reg trst;
-	wire cdr1;
-	wire sdr1;
-	wire udr1;
-	wire cir1;
-	wire sir1;
-	wire uir1;
-	tap_controller dut (
-		.tck(tck), 
-		.tms(tms), 
-		.trst(trst), 
-		.cdr1(cdr1), 
-		.sdr1(sdr1), 
-		.udr1(udr1), 
-		.cir1(cir1), 
-		.sir1(sir1), 
-    .uir1(uir1) ); 
-initial begin
-		tck = 0;
-		forever #10 tck = ~tck;
-end
-initial begin
-		tms = 0;
-		trst = 0;
-		#20;
-		trst = 1;
-    #20;
-		tms = 1;
-	  #20;
-	  tms = 0;
-    #20;	
-    tms = 0;
-	  #20;
-    tms = 0;
-		#20;
-		tms = 1;
-	  #20;
-	  tms = 1;
-		#20;
-		tms=0;
-		#20;
-		tms = 1;
-	  #20;
-	  tms = 1;
-		#20
-		tms = 0;
-		#20;
-    tms = 1;
-	  #10;
-	  tms = 0;
-    #20;	
-    tms = 0;
-	  #20;
-	  tms = 0; //repeat for shift high
-		#20;
-		tms = 1;
-	  #20;
-	  tms = 1; //udr1 is highr
-		#20;
-		tms=1;
-		#100;
-		tms=0;
-		#100;
-	end
+    reg tck;
+    reg tms;
+    reg trst_n;
+    wire [3:0] state;
+    tap_controller uut (
+        .tck(tck),
+        .tms(tms),
+        .trst_n(trst_n),
+        .state(state) );
+    initial begin
+        tck = 0;
+        forever #5 tck = ~tck;
+    end
+    initial begin
+        trst_n = 0;
+        tms = 1;
+        #20;
+        trst_n = 1;
+        #10;
+        tms = 0; 
+        #10;
+        tms = 1;
+        #10;
+        tms = 0; 
+        #10;
+        tms = 0;
+        #10;
+        tms = 1;
+        #10;
+        tms = 1;
+        #10;
+        tms = 0;
+        #10;
+        trst_n = 0;
+        #10;
+        trst_n = 1;
+        #10;
+        $finish;
+    end
 endmodule
